3b1f662e2c13b074af01791ef8ee2e9fa626ebeb
badandy-beep <badolatoandy@gmail.com>
2025-12-16 13:39:30 -0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+/* © 2025 Noetic Dharma Group, LLC | www.noeticdharma.com | CONFIDENTIAL & PROPRIETARY | Unauthorized use prohibited */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
